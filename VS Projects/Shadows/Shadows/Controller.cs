cc769925889d4817b953b9cc9f922e8ccef5426c
itsjohno <johnathanlaw@gmail.com>
2013-11-18 17:49:58 +0000

    Adding of Class Files

---
+++ VS Projects/Shadows/Shadows/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadows.Content
+{
+    /// <summary>
+    /// The controller class allows the game to accept input from a number of sources, including
+    /// gamepads and keyboards. Future implementations may allow use of a touch screen device.
+    /// </summary>
+    class Controller
+    {
+        
+    }
+}
